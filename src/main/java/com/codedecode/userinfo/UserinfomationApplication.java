9f020cc9c24d6ecd4940ddd606204f44d1684cdf
mori <mori@morinoiMac.local>
2024-08-27 13:03:27 +0900

    first commit

---
+++ src/main/java/com/codedecode/userinfo/UserinfomationApplication.java
@@ -0,0 +1,13 @@
+package com.codedecode.userinfo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserinfomationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UserinfomationApplication.class, args);
+	}
+
+}
